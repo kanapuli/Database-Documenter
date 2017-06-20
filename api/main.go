e72f45c82e50cb0765b14e1dabfd527ac639a588
kanapuliAthavan <athavankanapuli@gmail.com>
2017-06-20 08:26:54 +0530

    Added Login API in golang

--- api/main.go
+++ api/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
+	"log"
+	"net/http"
+
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	//"github.com/gorilla/mux"
-	"log"
+
+	"github.com/gorilla/mux"
 )
+
+const mongoUrl = "mongodb://127.0.0.1:27017"
 
 //Person - struct to hold the user details
 type Person struct {
@@ -16,25 +23,59 @@ type Person struct {
 	IsAdmin    bool
 	IsApprover bool
 }
+type DbResponse struct {
+	session *mgo.Session
+	err     error
+}
+type DbRequest struct {
+	url string
+}
 
 func main() {
-	session, err := mgo.Dial("mongodb://127.0.0.1:27017")
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/login", Login).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+type LoginDetails struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	//Get The Mongo Session First
+	dbClient := &DbRequest{}
+	dbClient.url = mongoUrl
+	response := dbClient.ConnectMongo()
+	if response.err != nil {
+		log.Fatal(response.err)
+	}
+	defer response.session.Close()
+	var Credentials LoginDetails
+	_ = json.NewDecoder(r.Body).Decode(&Credentials)
+
+	mongoRecord := Person{}
+	c := response.session.DB("mydb").C("users")
+	fmt.Println(Credentials)
+	err := c.Find(bson.M{"username": Credentials.Username, "password": Credentials.Password}).One(&mongoRecord)
 	if err != nil {
-		log.Fatal(err)
+		json.NewEncoder(w).Encode("{\"reponse\"Record Not found\"}")
+		return
+	}
+	json.NewEncoder(w).Encode(mongoRecord)
+
+}
+
+//ConnectMongo  -  Connects to mongo db using the receiver string and returns the pointer session and error
+func (request DbRequest) ConnectMongo() *DbResponse {
+	response := &DbResponse{}
+
+	response.session, response.err = mgo.Dial(request.url)
+	if response.err != nil {
+		log.Fatal(response.err)
 	}
 	fmt.Println("connected")
-	defer session.Close()
+	return response
 
-	c := session.DB("mydb").C("users")
-	/*err = c.Insert(&Person{"Athavan Kanapuli", "Kanapuli", "play2win", true, false})
-	if err != nil {
-		log.Fatal(err)
-	}
-	result := Person{}
-	err = c.Find(bson.M{"name":"Athavan Kanapuli"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(result)
-	*/
 }
